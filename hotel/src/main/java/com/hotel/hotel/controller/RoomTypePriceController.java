2f8a90993d1d5a715bc62fe519ba7680bc73782a
IT21302862 <sanayasamadhi@gmail.com>
2023-09-29 05:38:17 +0530

    add frontend access

--- hotel/src/main/java/com/hotel/hotel/controller/RoomTypePriceController.java
+++ hotel/src/main/java/com/hotel/hotel/controller/RoomTypePriceController.java
@@ -6,6 +6,7 @@ import com.hotel.hotel.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/hotels")
+@CrossOrigin
 public class RoomTypePriceController {
 
     @Autowired
